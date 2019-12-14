d3d4e67d73515eb39251052506001f9a1fc5012b
Ian <mus5002@aol.com>
2019-12-14 12:54:08 -0500

    projects router completed

--- routes/projectRouter.js
+++ routes/projectRouter.js
@@ -10,4 +10,48 @@ router.get('/', (req, res) => {
         .catch(err => next(err))
 })
 
+router.get('/:id', (req, res) => {
+    projects.get(req.params.id)
+        .then(project => {
+            if (!project) {
+                return res.status(404).json({ message: 'project not found'})
+            }
+            res.status(200).json(project)
+        })
+        .catch(err => next(err))
+})
+
+router.post('/', (req, res) => {
+    projects.insert(req.body)
+        .then(project => {
+            res.status(200).json(project)
+        })
+        .catch(err => next(err))
+})
+
+router.put('/:id', (req, res) => {
+    let id = req.params.id
+    let project = req.body
+
+    projects.update(id, project)
+        .then(project => {
+            if (project === null) {
+                res.status(404).json({ message: 'project not found'})
+            }
+            res.status(200).json(project)
+        })
+        .catch(err => next(err))
+})
+
+router.delete('/:id', (req, res) => {
+    projects.remove(req.params.id)
+        .then(deleted => {
+            if (deleted === 0) {
+                res.status(404).json({ message: 'project not found'})
+            }
+            res.status(200).json({ message: 'project deleted'})
+        })
+        .catch(err => next(err))
+})
+
 module.exports = router
